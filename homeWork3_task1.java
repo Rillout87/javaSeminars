f654d007c89590c7e07ceddd7f7c7aaca23f6599
Rillout87 <100080112+Rillout87@users.noreply.github.com>
2022-08-21 21:59:44 +0300

    Update homeWork3_task1.java

--- homeWork3_task1.java
+++ homeWork3_task1.java
@@ -1,64 +1,63 @@
 //На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
-public class homeWork3 {
+import java.util.ArrayList;
 
-    
-    static int[][] moves = new int[][]{{2, 1, -1, -2, -2, -1, 1, 2}, {1, 2, 2, 1, -1, -2, -2, -1}};
+public class homeWork3 {
 
     public static void main(String[] args) {
 
-        int[][] board = new int[8][8];
+        ArrayList<Integer> leftD = new ArrayList<>();
+        ArrayList<Integer> rightD = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> blockList = new ArrayList<>(2);
+        blockList.add(leftD);
+        blockList.add(rightD);
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = 0;
-            }
-        }
-
-        board[0][0] = 1; // стартуем с точки (0,0)
-        if (!findBoard(0, 0, 2, board)) {
-            System.out.println("решения нет");
-        } else {
-            printArr(board);
-        }
+        ArrayList<Integer> curRes = new ArrayList<Integer>();
+        combine(0, 0, blockList, curRes);
     }
 
-    public static boolean validPos(int curRow, int curCol, int[][] currBoard) {
-
-        if ((curRow >= 0) && (curCol >= 0) && (curRow < 8) && (curCol < 8)
-                && (currBoard[curRow][curCol] == 0)) {
-            return true;//ход в пределах доски и ячейка не занята
-        }
-        return false;
-    }
-
-    public static boolean findBoard(int curRow, int curCol, int count, int[][] currBoard) {
-        if (count == 65) {
+    public static boolean validPos(int numRow, int numCol, ArrayList<ArrayList<Integer>> check) {
+        
+        if ((check.get(0).indexOf(numRow + numCol) == -1) && (check.get(1).indexOf(numRow - numCol + 8) == -1)) {
             return true;
         }
-        for (int currMove = 0; currMove < 8; currMove++) {
-            int nextRow = curRow + moves[0][currMove];
-            int nextCol = curCol + moves[1][currMove];
-
-            if (validPos(nextRow, nextCol, currBoard)) {
-                currBoard[nextRow][nextCol] = count;
-                if (findBoard(nextRow, nextCol, count + 1, currBoard)) {
-                    return true;
-                } else {
-                    currBoard[nextRow][nextCol] = 0;
-                }
-            }
-        }
         return false;
     }
 
-    public static void printArr(int[][] arr) {
-        System.out.println("print Board");
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                System.out.print(arr[i][j] + "\t");
+    public static ArrayList<Integer> combine(int curRow, int curCol, ArrayList<ArrayList<Integer>> blockList, ArrayList<Integer> curRes) {
+        if (curRes.size() == 8) {
+            printList(curRes);
+            return curRes;
+        }
+        if (curRow == 8)
+        {
+            int temp = curRes.remove(curRes.size() - 1);
+            blockList.get(0).remove(curRes.size());
+            blockList.get(1).remove(curRes.size());
+            combine(temp + 1, curCol - 1, blockList, curRes);
+            return curRes;
+        }
+
+        if (validPos(curRow, curCol, blockList) && (curRes.indexOf(curRow) == -1)) {
+            curRes.add(curRow);
+            blockList.get(0).add(curRow + curCol);
+            blockList.get(1).add(curRow - curCol + 8);
+            combine(0, curCol + 1, blockList, curRes);
+        }
+        if (curRes.size() < 8) {
+            combine(curRow + 1, curRes.size(), blockList, curRes);
+        }
+        return curRes;
+    }
+
+    public static void printList(ArrayList<Integer> arrList) {
+        int sizeBoard = arrList.size();
+        System.out.println("Итоговая доска:");
+        for (int item : arrList) {
+            for (int currCell = 0; currCell < sizeBoard; currCell++) {
+                if (currCell == item) System.out.print(("Q") + "\t");
+                else System.out.print((".") + "\t");
             }
             System.out.println();
         }
     }
-
 }
